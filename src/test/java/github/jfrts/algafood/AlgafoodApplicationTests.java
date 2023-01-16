b5432128d57bbd59ad38939f16ad7986f040829c
Júlio Freitas <jcsr.frts@gmail.com>
2023-01-16 15:08:20 -0300

    Spring initializr

---
+++ src/test/java/github/jfrts/algafood/AlgafoodApplicationTests.java
@@ -0,0 +1,13 @@
+package github.jfrts.algafood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgafoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
