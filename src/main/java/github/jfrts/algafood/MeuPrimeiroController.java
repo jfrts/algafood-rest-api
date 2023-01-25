4277c95b77ea5eb9ef40c3e80d2fca70358cf507
Júlio Freitas <jcsr.frts@gmail.com>
2023-01-25 14:04:31 -0300

    test controller

---
+++ src/main/java/github/jfrts/algafood/MeuPrimeiroController.java
@@ -0,0 +1,16 @@
+package github.jfrts.algafood;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MeuPrimeiroController {
+    
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "Hello friend...";
+    }
+
+}
